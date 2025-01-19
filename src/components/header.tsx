f45e2ccecad558a82b417bfe51bb5aaa012551b7
eliah <eliah.aquilina@gmail.com>
2025-01-19 15:12:10 -0600

    Update header to include new mobile drawer

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,31 +1,29 @@
 import { NavLink } from 'react-router'
-import { Menu } from 'lucide-react'
 import { SiGithub } from '@icons-pack/react-simple-icons'
-import ThemeToggle from '@/components/theme-toggle.tsx'
+import ThemeToggle from '@/components/theme-toggle'
+import Drawer from '@/components/drawer'
 
 function Header() {
     return (
         <>
             <header className='sticky top-0 flex w-full justify-between border-b border-b-border p-2 backdrop-blur'>
+                <div className='md:hidden'>
+                    <Drawer />
+                </div>
                 <div className='flex items-center justify-center pl-2'>
                     <h3>Aquilina</h3>
                 </div>
-                <div className='flex items-center justify-center'>
-                    <nav className='hidden space-x-4 md:block'>
-                        <NavLink to='/#about'>About</NavLink>
-                        <NavLink to='/'>Experience</NavLink>
-                        <NavLink to='/'>Projects</NavLink>
-                        <NavLink to='/'>Hobbies</NavLink>
-                    </nav>
-                </div>
+                <nav className='jutify-center hidden items-center space-x-4 md:flex'>
+                    <NavLink to='/#about'>About</NavLink>
+                    <NavLink to='/'>Experience</NavLink>
+                    <NavLink to='/'>Projects</NavLink>
+                    <NavLink to='/'>Hobbies</NavLink>
+                </nav>
                 <div className='flex flex-row items-center justify-center gap-2'>
                     <div className='hidden md:block'>
                         <SiGithub />
                     </div>
                     <ThemeToggle />
-                    <div className='md:hidden'>
-                        <Menu />
-                    </div>
                 </div>
             </header>
         </>
