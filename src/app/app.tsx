943efbe39d2620787599c651f6ec9862bafeccb2
eliah <eliah.aquilina@gmail.com>
2025-01-28 01:10:11 -0600

    Remove react router, add react typed and eslint import

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,18 +1,10 @@
-import { BrowserRouter, Route, Routes } from 'react-router'
 import Home from '@/app/routes/home.tsx'
 import Header from '@/components/header.tsx'
 
 function App() {
     return (
         <>
-            <BrowserRouter>
-                <div>
-                    <Header />
-                    <Routes>
-                        <Route path='/' element={<Home />} />
-                    </Routes>{' '}
-                </div>
-            </BrowserRouter>
+            <Home />
         </>
     )
 }
