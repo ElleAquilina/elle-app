c4bb68db89bb9effc63a68c0bec3dea10dd427ce
eliah <eliah.aquilina@gmail.com>
2025-01-30 14:45:50 -0600

    Upgrade to tailwind 4.0

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite'
 import { fileURLToPath, URL } from 'url'
 import react from '@vitejs/plugin-react-swc'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-    plugins: [react()],
+    plugins: [react(), tailwindcss()],
     resolve: {
         alias: [
             {
